cc78040a139b59280a58e083535514557b661b22
armenskixyz <conk.dsc@onet.pl>
2025-09-07 17:38:10 +0200

    initial

---
+++ fish/config.fish
@@ -0,0 +1,37 @@
+# Fish shell equivalent of zsh-newuser-install config
+set -Ux HISTFILE ~/.histfile
+set -Ux HISTSIZE 1000
+set -Ux SAVEHIST 1000
+
+# Fish automatically supports "autocd"-like behavior
+
+set -U fish_greeting ""
+
+# Aliases
+alias cd='z'
+alias ..='cd ..'
+alias ff='fastfetch'
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+alias vim='nvim'
+
+# Prompt (Fish uses its own prompt system; use Starship or customize below)
+# If using Starship for prompt:
+starship init fish | source
+
+# If you want a simple similar prompt manually (optional alternative):
+# function fish_prompt
+#     echo -n '['(whoami)'@'(hostname)' '(prompt_pwd)']$ '
+# end
+
+# Pyenv init
+status --is-interactive; and pyenv init - | source
+status --is-interactive; and pyenv virtualenv-init - | source
+
+# Custom paths
+set -x PATH $PATH /home/armen/waybar/build/
+set -x PATH $PATH /home/armen/rb-scrobbler/build/
+set -x PATH $PATH /home/armen/go/bin/
+set -gx QT_QPA_PLATFORMTHEME kde
+
+zoxide init fish | source
